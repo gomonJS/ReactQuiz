20d64708ff226d6e45bbd54f970650e474424c42
Александр Гомон <28gomon@mail.ru>
2020-05-01 16:44:55 +0300

    Добавлен компонент ActiveQuiz

---
+++ react-quiz/src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './ActiveQuiz.module.css';
+
+const ActiveQuiz = props => {
+
+    return (
+        <div className={classes.ActiveQuiz}>
+            <div className={classes.Question}>
+                
+            </div>
+        </div>
+    );
+}
+
+export default ActiveQuiz;

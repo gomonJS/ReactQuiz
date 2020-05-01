19c440553cfd5e6a1875e6b9801013b11ea6b112
Александр Гомон <28gomon@mail.ru>
2020-05-01 23:24:21 +0300

    Навигация, иконка меню гамбургер. MenuToggle
    Компонента переключения меню MenuToggle дабавлена в Layout

--- react-quiz/src/hoc/Layout/Layout.js
+++ react-quiz/src/hoc/Layout/Layout.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import classes from './Layout.module.css';
+import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle";
 
 export default class Layout extends React.Component {
+
+    state = {
+        menu: false
+    }
+
+    onMenuClick = () => {
+        this.setState({
+            menu: !this.state.menu
+        });
+    }
 
     render() {
         return (
             <div className={classes.Layout}>
+
+                <MenuToggle
+                    isOpen={this.state.menu}
+                    onMenuClick={this.onMenuClick}
+                />
+
                 <main>
                     {this.props.children}
                 </main>

a20939ec1639a1519e9bbfffbb82c9cf878047d3
Александр Гомон <28gomon@mail.ru>
2020-05-01 16:38:17 +0300

    Добавлен контейнер Quiz

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Layout from "./hoc/Layout/Layout";
+import Quiz from "./containers/Quiz/Quiz";
 
 function App() {
     return (
         <div className="App">
             <Layout>
+
+                <Quiz/>
 
             </Layout>
         </div>
---
+++ react-quiz/src/containers/Quiz/Quiz.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Quiz.module.css';
+
+export default class Quiz extends React.Component {
+
+    render() {
+        return (
+            <div className={classes.Quiz}>
+                <div className={classes.QuizWrapper}>
+                    <h1></h1>
+                </div>
+            </div>
+        );
+    }
+}

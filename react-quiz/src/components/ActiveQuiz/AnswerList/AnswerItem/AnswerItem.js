b1a7f0aa9a1be981fec66029b828303706ab2cb0
Александр Гомон <28gomon@mail.ru>
2020-05-01 16:55:46 +0300

    Компонента AnswerItem
    Компонента выводит варианты ответов теста

---
+++ react-quiz/src/components/ActiveQuiz/AnswerList/AnswerItem/AnswerItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './AnswerItem.module.css';
+
+const AnswerItem = props => {
+
+    return (
+        <li className={classes.AnswerItem}>
+
+        </li>
+    );
+}
+
+export default AnswerItem;
--- react-quiz/src/components/ActiveQuiz/AnswerList/AnswerList.js
+++ react-quiz/src/components/ActiveQuiz/AnswerList/AnswerList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './AnswerList.module.css';
+import AnswerItem from "./AnswerItem/AnswerItem";
 
 const AnswerList = props => {
 
     return (
         <ul className={classes.AnswerList}>
-
+            <AnswerItem/>
         </ul>
     );
 }

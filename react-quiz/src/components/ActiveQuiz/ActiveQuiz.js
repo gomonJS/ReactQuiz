8e13cfe121fe188cd3fc3c8a63bcf2a1ca197df7
Александр Гомон <28gomon@mail.ru>
2020-05-01 16:52:21 +0300

    Компонент AnswerList

--- react-quiz/src/components/ActiveQuiz/ActiveQuiz.js
+++ react-quiz/src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './ActiveQuiz.module.css';
 import Title from "../UI/Title/Title";
+import AnswerList from "./AnswerList/AnswerList";
 
 const ActiveQuiz = props => {
 
@@ -8,6 +9,8 @@ const ActiveQuiz = props => {
         <div className={classes.ActiveQuiz}>
             <div className={classes.Question}>
                 <Title styleTitle={classes.Title}>Question</Title>
+
+                <AnswerList/>
             </div>
         </div>
     );
---
+++ react-quiz/src/components/ActiveQuiz/AnswerList/AnswerList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './AnswerList.module.css';
+
+const AnswerList = props => {
+
+    return (
+        <ul className={classes.AnswerList}>
+
+        </ul>
+    );
+}
+
+export default AnswerList;
